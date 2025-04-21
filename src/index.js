ef769443897a4d6d04e42f397143b60b92b550ad
sahak <yso0204@gmail.com>
2025-04-21 21:48:35 +0900

    SPA 구현

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
+//SPA로 구현할때
 const API_URL = 'https://animal-api-two.vercel.app/';
-//사진을 넣기 위해 content class 를 찾고
 const $content = document.querySelector('div.content');
-//사진을 여러개 넣어서 전달하기 위한 빈 문자열 선언
-let template = "";
-const getData = async () => {
-    let res = await fetch(API_URL);
-    //api를 비동기 적으로 처리하기 위해서 asyn 함수로 구현
+const $tabs = document.querySelectorAll('.tab');
+
+const getData = async (animal) => {
     try {
+        const url = animal === 'all' ? API_URL : `${API_URL}${animal}`;
+        let res = await fetch(url);
         if (res) {
             let data = await res.json();
+            $content.innerHTML = ""; //이전 내용 비우기
             data.photos.forEach(element => {
-                // template += `<img src = ${element.url}></img>`
-                //innerHTML로 넣어도 됨
-                // $content.innerHTML = template;
-                //보안으로 인해 추천하지 않으므로 다른거로
                 const img = document.createElement('img');
                 img.src = element.url;
                 $content.appendChild(img);
             });
         }
     }
-    catch (err){
+    catch (err) {
         console.log(err);
     }
-}
+};
 
-getData();
+//tab 클릭 이벤트 구현
+$tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        $tabs.forEach((t) => t.classList.remove('clicked'));
+        tab.classList.add('clicked');
+
+        const animal = tab.dataset.animal;
+        getData(animal);
+    })
+});
+
+getData('all');
